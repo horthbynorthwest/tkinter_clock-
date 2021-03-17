4eff5eeecababc17c52d3ad599b7e68f64dc1649
Georgie Horth <georgiehorth@gmail.com>
2021-03-17 10:01:05 +0000

    Created a cooking conversion gui.

--- gui.py
+++ gui.py
@@ -4,18 +4,19 @@ window = Tk()
 
 def km_to_miles():
     miles = float(e1_value.get()) * 1.6
+    t1.delete("1.0", END)
     t1.insert(END, miles)
 
 b1 = Button(window, text="Convert", command=km_to_miles)
 b1.grid(row=0,column=0)
 
-km_label = Label(text="KM")
+km_label = Label(text="Miles")
 km_label.grid(row=0, column=1)
 e1_value = StringVar()
 e1 = Entry(window, textvariable=e1_value)
 e1.grid(row=1, column=1)
 
-miles_label = Label(text="Miles")
+miles_label = Label(text="Km")
 miles_label.grid(row=0, column=2)
 t1 = Text(window, height=1, width=20)
 t1.grid(row=1, column=2)
