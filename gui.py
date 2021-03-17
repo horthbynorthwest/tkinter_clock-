2dec8ace8ac68f7e5d210324fff8d09a69c95248
Georgie Horth <georgiehorth@gmail.com>
2021-03-17 09:18:23 +0000

    Run button now displays Success! in the text element

--- gui.py
+++ gui.py
@@ -3,12 +3,13 @@ from tkinter import *
 window = Tk()
 
 def km_to_miles():
-    t1.insert(END,"Success!")
+    t1.insert(END,e1_value.get())
 
 b1 = Button(window,text="Run",command=km_to_miles)
 b1.grid(row=0,column=0)
 
-e1=Entry(window)
+e1_value = StringVar()
+e1=Entry(window,textvariable=e1_value)
 e1.grid(row=0,column=1)
 
 t1=Text(window,height=1,width=20)
